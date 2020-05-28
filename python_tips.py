7fb8090ad9e1d1602665d53f39a5701576f08012
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-05-28 07:20:45 -0700

    Update python_tips.py

--- python_tips.py
+++ python_tips.py
@@ -35,10 +35,36 @@ This should be aplicable to other data types too
 ======================================
 List(Mutable)
 ======================================
+List is mutable object in python, so you don't need to pass list
+defined in the parent function to the child function
+
+def parent():
+  res = []
+  def child():
+    res.append(1)
+    
+ This works fine. remember this will not work for normal variable. 
+you can still read the variable but can't write it as it is immutable
+
+----------------------------------------------- 
 to allocate row*col 2d array in python
 res = [[-1 for _ in range(col)] for _ in range(row)]
------------------------------------------------  
 
+----------------------------------------------- 
+To append an element at given index in list, use a.insert(index, val)
+a = [2,3,4]
+a.insert(0, 1)
+a = [1,2,3,4]
+
+----------------------------------------------- 
+to check if the given object is a list or not in python is isinstance(x, list)
+a = [1,2,3]
+if isinstance(a, list):
+    print('yes')
+else:
+    print('no')
+    
+-----------------------------------------------  
 You can merge two list just like strings using + operator
 a = [1, 2]
 b = [3, 4]
