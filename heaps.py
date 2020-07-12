af1b01e039a26c5dbff933bb26c302ea59c1aaef
yastil01 <63788022+yastil01@users.noreply.github.com>
2020-07-12 00:14:30 -0700

    Update heaps.py

--- heaps.py
+++ heaps.py
@@ -1,7 +1,27 @@
 ======================================
 Heaps(Mutable)
 ======================================
+you need to import the following to use heaps in python
+from heapq import heappush, heappop
+
+by default heap is a min heap in python
+---------------------------------------
 heap = []
 
 heappush(heap, x)
 heappop(heap)
+---------------------------------------
+You need to use min Heap to find k largest elements and max heap for 
+k smallest elements
+
+for example
+a = [1,2,3,4,5]
+minHeap = []
+
+for num in a:
+    heappush(minHeap, num)
+    if len(minHeap) > 2:
+        heappop(minHeap)
+        
+print(minHeap) -> [4,5]
+
